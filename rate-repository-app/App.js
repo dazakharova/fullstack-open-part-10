fedbfdd890cefea75d56ba3bcab6b075bbe36682
Daria Zakharova <e2402638@edu.vamk.fi>
2026-08-15 22:20:18 +0300

    Replace REST API with GraphQL queries (ex.11)

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,19 @@
 import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client/react';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
     <>
       <StatusBar />
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
     </>
   )
---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,21 @@
+import { useQuery } from '@apollo/client/react';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  if (error) {
+    console.error('Error fetching repositories:', error);
+  }
+
+  return {
+    repositories: data ? data.repositories : null,
+    loading,
+    refetch,
+  };
+};
+
+export default useRepositories;
